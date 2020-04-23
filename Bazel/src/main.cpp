2603a7f93fd930d00f2588458dc47d55c1fba858
Zvonimir Pervan <z.pervan65@gmail.com>
2020-04-23 17:43:45 +0200

    Basic repo file structure

---
+++ Bazel/src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by zperv on 4/22/2020.
+//
+#include <iostream>
+
+int main()
+{
+  std::cout << R"rawr( Hellou! )rawr";
+}
---
+++ CMake/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "'sup, World!" << std::endl;
+
+    return 0;
+}
